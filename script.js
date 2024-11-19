89ec7dd98321964038e36c8d2cc97e6a873a59f0
David <davidifme@icloud.com>
2024-11-19 14:41:46 +0100

    Add drawing functionality

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const divContainer = document.querySelector("#div-container");
 
 for (let index = 0; index < 256; index++) {
     const div = document.createElement("div");
+    div.addEventListener("mousemove", () => {
+        div.style.backgroundColor = "white";
+    });
     divContainer.appendChild(div);
 }
