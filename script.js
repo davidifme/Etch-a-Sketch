3f5b6466358279b4940035585b890291bd86e2d3
David <davidifme@icloud.com>
2024-11-19 15:15:19 +0100

    Add grid creating button functionality

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 const divContainer = document.querySelector("#div-container");
+const btn = document.querySelector(".btn");
 
-for (let index = 0; index < 256; index++) {
-    const div = document.createElement("div");
-    div.addEventListener("mousemove", () => {
-        div.style.backgroundColor = "white";
+btn.addEventListener("click", () => {
+
+    const gridToRemove = document.querySelectorAll("#div-container div");
+    gridToRemove.forEach((grid) => {
+        grid.remove();
     });
-    divContainer.appendChild(div);
-}
+
+    let maxGridSize = 50;
+    let rowNumber = 0;
+    let columnNumber = 0;
+
+    let isRunning = true;
+
+    while(isRunning) {
+        let userRowNumberInput = prompt("Number of rows:");
+        let userColumnNumberInput = prompt("Number of columns:");
+
+        if (userRowNumberInput <= maxGridSize && userColumnNumberInput <= maxGridSize) {
+            if (userRowNumberInput >= 1 || userColumnNumberInput >= 1) {
+                rowNumber = userRowNumberInput;
+                columnNumber = userColumnNumberInput;
+                break;
+            } else {
+                alert('ERROR! Enter a number greater than 0.')
+                continue;
+            }
+        } else {
+            alert('ERROR! The maximum grid size is 50.')
+            continue;
+        }
+    }
+
+    let rowsAndColumnsNumber = rowNumber * columnNumber;
+
+    for (let index = 0; index < rowsAndColumnsNumber; index++) {
+        const div = document.createElement("div");
+        div.addEventListener("mousemove", () => {
+            div.style.backgroundColor = "white";
+        });
+        divContainer.appendChild(div);
+    }
+
+    let maxWidth = (columnNumber * 25) + 20;
+    divContainer.style.maxWidth = `${maxWidth}px`;
+});
